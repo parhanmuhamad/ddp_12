d689538a45f2d9230fd9d2d825785af37d4f337c
parhanmuhamad <parhanmuhamad30@gmail.com>
2024-11-20 15:31:52 +0700

    Muhamadparhan_0110124047_ddp_9

---
+++ ddp-9.py
@@ -0,0 +1,40 @@
+
+def celcius_ke_fahrenheit(celcius):
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit 
+print(celcius_ke_fahrenheit(0))
+print(celcius_ke_fahrenheit(100))
+
+print('n\---Mencari bilangan genap')
+def is_genap(bil_genap):
+    return bil_genap %2 == 0
+
+# untuk memasukan value
+print(is_genap(4))
+print(is_genap(7))
+
+#rata rata nilai 70
+def nilai_kelulusan(nilai):
+    if nilai >= 80:
+        return 'lulus'
+    else :
+        return 'gagal'
+    
+#untuk mencetak value
+print(nilai_kelulusan(80))
+print(nilai_kelulusan(60))
+
+print('\n----cetak bilangan ganjil----')
+def bil_ganjil(angka):
+    for i in range(1, angka, 2):
+        print(i, end=" ")
+
+#untuk memasukan value
+bil_ganjil(20)
+
+
+
+
+
+
+
