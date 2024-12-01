01c1a7619820504fd3d4d366ea1d80a860a7fefb
parhanmuhamad <parhanmuhamad30@gmail.com>
2024-12-01 19:14:02 +0700

    tugasDdp_Pekan10

---
+++ call.py
@@ -0,0 +1,23 @@
+import bangundatar
+
+bangundatar.l_persegi(5)
+bangundatar.l_persegi_panjang(10,5)
+bangundatar.l_segitiga(2,8)
+bangundatar.l_lingkaran(1)
+bangundatar.l_jajargenjang(10,15)
+
+import bangunruang
+
+bangunruang.l_balok(4,9,2)
+bangunruang.l_kubus(6)
+bangunruang.l_tabung(8,6)
+bangunruang.l_limas_segitiga(3,5)
+bangunruang.l_prisma_segitiga(2,4)
+
+import hitung
+
+hitung.tambah(2, 4)
+hitung.kurang(8, 6)
+hitung.kali(5, 5)
+hitung.bagi(16, 4)
+hitung.pangkat(5, 3)
---
+++ hitung.py
@@ -0,0 +1,21 @@
+import math
+
+def tambah(bil1, bil2):
+    hasil = 2 + 4
+    print("Hasil Tambah Dari",2, "+", 4, "=", hasil)
+
+def kurang(bil1, bil2):
+    hasil = 8 - 6
+    print("Hasil Pengurangan Dari",8, "-", 6, "=", hasil)
+
+def kali(bil1, bil2):
+    hasil = 5 * 5
+    print("Hasil Perkalian Dari",5, "*", 5, "=", hasil)
+
+def bagi(bil1, bil2):
+    hasil = 16 / 4
+    print("Hasil Pembagian Dari",16, "/", 4, "=", hasil)
+
+def pangkat(bil1, bil2):
+    hasil = math.pow(5, 3)
+    print("Hasil Pemangkatan Dari",5, "^", 3, "=", hasil)
